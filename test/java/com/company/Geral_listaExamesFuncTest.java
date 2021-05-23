952e3de90eb66016a4a79bcc8cf21b77ab013f0d
Flávio Oliveira <flaviomcoliveira@gmail.com>
2021-05-23 12:34:51 +0100

    mod(Geral_listaExamesFuncTest): created 2 test cases to test if the read and write from jUnit work corretly.

--- test/java/com/company/Geral_listaExamesFuncTest.java
+++ test/java/com/company/Geral_listaExamesFuncTest.java
@@ -2,11 +2,68 @@ package com.company;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Geral_listaExamesFuncTest {
 
+    private final InputStream systemIn = System.in;
+    private final PrintStream systemOut = System.out;
+    private ByteArrayInputStream testIn;
+    private ByteArrayOutputStream testOut;
+
+    @BeforeEach
+    public void setUpOutput() {
+        testOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testOut));
+    }
+
+    @AfterEach
+    public void restoreSystemInputOutput() {
+        System.setIn(systemIn);
+        System.setOut(systemOut);
+    }
+
+    private void provideInput(String data) {
+        testIn = new ByteArrayInputStream(data.getBytes());
+        System.setIn(testIn);
+    }
+
     @Test
-    void listaExamesFunc() {
+    void listaExamesFuncA() {
+        String expected1 = "Num do Funcionario :"; //this is start of expected1 string
+        String expected2 = "Deseja que o resultado seja escrito num ficheiro?(0-n/1-n):";
+        String input1 = "17";
+        String input2 = "0";
+
+        Geral geral = new Geral();
+        geral.listaExamesFunc();
+
+        assert(testOut.toString().contains(expected1));
+        assert(testOut.toString().contains(expected2));
+
+    }
+    @Test
+    void listaExamesFuncB() {
+        String expected1 = "Num do Funcionario :"; //this is start of expected1 string
+        String expected2 = "Deseja que o resultado seja escrito num ficheiro?(0-n/1-n):";
+        String input1 = "17";
+        String input2 = "0";
+
+        provideInput(input1);
+
+        Geral geral = new Geral();
+        geral.listaExamesFunc();
+
+        assert(testOut.toString().contains(expected1));
+        assert(testOut.toString().contains(expected2));
+
     }
 }
